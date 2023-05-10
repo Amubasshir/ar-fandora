5262643f3e12192e527555cb3705a8ce245d0fc0
Mubasshir Ahmed <lastmubasshir@gmail.com>
2023-05-10 12:07:33 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="text-7xl">heloo , from fandome...</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
