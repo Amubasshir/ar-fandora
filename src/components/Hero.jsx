d3c44e5f1e351634b8d5cffab547ddc651fee1f9
Mubasshir Ahmed <lastmubasshir@gmail.com>
2023-05-13 10:44:38 +0600

    button animation added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -22,7 +22,11 @@ const Hero = () => {
             <br /> <span>and Content Marketplace</span>
           </p>
 
-          <button className="relative box-border flex items-center justify-center mt-5 border-2 ml-2 border-teal-500 text-[#52C1B9] rounded-full font-montserrat text-sm leading-7 w-32 h-9 ">
+          {/* <button className="relative box-border flex items-center justify-center mt-5 border-2 ml-2 border-teal-500 text-[#52C1B9] rounded-full font-montserrat text-sm leading-7 w-32 h-9 ">
+            <span className="absolute left-3 w-[11px] h-[11px] rounded-full bg-teal-500"></span>
+            Now Live
+          </button> */}
+          <button className="relative box-border flex items-center justify-center mt-5 border-2 ml-2 border-teal-500 text-[#52C1B9] rounded-full font-montserrat text-sm leading-7 w-32 h-9 blink">
             <span className="absolute left-3 w-[11px] h-[11px] rounded-full bg-teal-500"></span>
             Now Live
           </button>
