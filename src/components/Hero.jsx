837926edf95e91a1af4e0fc1e1cc872e43c8f85c
Mubasshir Ahmed <lastmubasshir@gmail.com>
2023-05-14 07:49:07 +0600

    desktop version done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,3 +1,42 @@
+// import heroBg from '../assets/HeroBg.png';
+// import secondheroBg from '../assets/f.png';
+// import Navbar from './Navbar';
+
+// const Hero = () => {
+//   return (
+//     <section
+//       className="h-screen max-w-[1440px] bg-no-repeat bg-center "
+//       style={{
+//         backgroundImage: `url(${heroBg})`,
+//         backgroundSize: '100% 100%',
+//       }}
+//     >
+//       <Navbar />
+//       <div className=" hero-section flex sm:flex-row">
+//         <div className="ml-16 mt-7">
+//           <h1 className="text-white sm:text-[140px] text-6xl uppercase  font-normal leading-[126px] font-bebas max-w-[606px]">
+//             Calling <br /> Creators of <br /> the Future
+//           </h1>
+
+//           <p className="font-montserrat  font-semibold text-2xl mt-6 leading-none ml-2 text-[#52C1B9]">
+//             <span>The World’s First Pre-NFT Rights</span>
+//             <br /> <span>and Content Marketplace</span>
+//           </p>
+//           <button className="relative box-border flex items-center justify-center mt-5 border-2 ml-2 border-teal-500 text-[#52C1B9] rounded-full font-montserrat text-sm leading-7 w-32 h-9 blink">
+//             <span className="absolute left-3 w-[11px] h-[11px] rounded-full bg-teal-500"></span>
+//             Now Live
+//           </button>
+//         </div>
+//         <div>
+//           <img src={secondheroBg} alt="" className="ml-32 " />
+//         </div>
+//       </div>
+//     </section>
+//   );
+// };
+
+// export default Hero;
+
 import heroBg from '../assets/HeroBg.png';
 import secondheroBg from '../assets/f.png';
 import Navbar from './Navbar';
@@ -12,11 +51,12 @@ const Hero = () => {
       }}
     >
       <Navbar />
-      <div className=" hero-section flex sm:flex-row">
+      <div className="hero-section flex sm:flex-row">
         <div className="ml-16 mt-7">
-          <h1 className="text-white text-[140px] uppercase  font-normal leading-[126px] font-bebas max-w-[606px]">
-            Calling <br /> Creators of <br /> the Future
+          <h1 className="text-white sm:text-[140px] text-6xl uppercase font-normal leading-[126px] font-bebas max-w-[606px] sm:max-w-[100%] sm:text-break-word sm:text-nowrap">
+            Calling Creators of the Future
           </h1>
+
           <p className="font-montserrat  font-semibold text-2xl mt-6 leading-none ml-2 text-[#52C1B9]">
             <span>The World’s First Pre-NFT Rights</span>
             <br /> <span>and Content Marketplace</span>
@@ -27,7 +67,7 @@ const Hero = () => {
           </button>
         </div>
         <div>
-          <img src={secondheroBg} alt="" className="ml-32 " />
+          <img src={secondheroBg} alt="" className="ml-32 md:ml-0" />
         </div>
       </div>
     </section>
